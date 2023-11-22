379051694b2cfb49207cc5f0ad5971179d37b12c
JORGE SANTACRUZ <santacruz.leonel.1h@hotmail.com>
2023-11-21 19:00:29 -0700

    Primer commit

---
+++ common.js
@@ -0,0 +1,11 @@
+const variable1 = function(){
+    console.log('Hola function 1')
+}
+const variable2 = function(){
+    console.log('Hola function 2')
+}
+
+module.exports = {
+    variable1,
+    variable2
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const a = require('./common.js')
+
+a.variable1();
+a.variable2();
